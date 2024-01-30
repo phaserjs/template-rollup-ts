4901ce32e3f2f4670520a105cd0c66ffa620aa6d
Francisco Pereira Alvarado <gammafp@gmail.com>
2024-01-30 13:37:39 +0100

    init

---
+++ rollup/rollup.config.dev.mjs
@@ -0,0 +1,80 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from '@rollup/plugin-commonjs';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import del from 'rollup-plugin-delete';
+import typescript from '@rollup/plugin-typescript';
+import url from '@rollup/plugin-url';
+import glslify from 'rollup-plugin-glslify';
+import { fileURLToPath } from 'node:url';
+import copy from 'rollup-plugin-copy';
+import alias from '@rollup/plugin-alias'
+import path from 'path';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+export default {
+    input: 'src/main.ts',
+    output: {
+        file: './dist/bundle.js',
+        name: 'PhaserTemplate',
+        format: 'iife',
+        indent: false,
+        sourcemap: true,
+        minifyInternalExports: false,
+    },
+    plugins: [
+        url({
+            emitFiles: true
+        }),
+        typescript(),
+        alias({
+            entries: [
+                { 
+                    find: 'phaser',
+                    replacement: path.resolve(__dirname, '../node_modules/phaser/dist/phaser.min.js')
+                }
+            ]
+        }),
+        
+        glslify(),
+        nodeResolve({
+            extensions: ['ts', 'tsx'],
+        }),
+        commonjs({
+            include: [
+                'node_modules/eventemitter3/**',
+                'node_modules/phaser/**'
+            ],
+            exclude: [
+                'node_modules/phaser/src/polyfills/requestAnimationFrame.js',
+                'node_modules/phaser/src/phaser-esm.js'
+            ],
+            sourceMap: true,
+            ignoreGlobal: false
+        }),
+        serve({
+            open: true,
+            contentBase: ['public', 'dist'],
+            host: 'localhost',
+            port: 8080,
+            verbose: false,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        }),
+        livereload({
+            watch: 'dist'
+        }),
+        del({
+            targets: 'dist/*',
+            runOnce: true
+        }),
+        copy({
+            targets: [
+                { src: 'index.html', dest: 'dist/' },
+            ],
+            copyOnce: true
+        })
+    ]
+};
---
+++ rollup/rollup.config.prod.mjs
@@ -0,0 +1,76 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from '@rollup/plugin-commonjs';
+import del from 'rollup-plugin-delete';
+import { babel } from '@rollup/plugin-babel';
+import typescript from '@rollup/plugin-typescript';
+import copy from 'rollup-plugin-copy';
+import url from '@rollup/plugin-url';
+import glslify from 'rollup-plugin-glslify';
+
+// Production
+import terser from '@rollup/plugin-terser';
+
+const phasermsg = () => {
+    return {
+        name: 'phasermsg',
+        buildStart() {
+            process.stdout.write(`Building for production...\n`);
+        },
+        buildEnd() {
+            const line = "---------------------------------------------------------";
+            const msg = `💜💜💜 Tell us about your game! - games@phaser.io 💜💜💜`;
+            process.stdout.write(`${line}\n${msg}\n${line}\n`);
+        }
+    }
+}   
+
+export default {
+    input: 'src/main.ts',
+    output: {
+        file: './dist/bundle.js',
+        name: 'PhaserTemplate',
+        format: 'iife',
+        sourcemap: true
+    },
+    plugins: [
+        glslify(),
+        nodeResolve({
+            browser: true,
+            extensions: ['ts', 'tsx'],
+        }),
+        typescript(),
+        commonjs({
+            include: [
+                'node_modules/eventemitter3/**',
+                'node_modules/phaser/**'
+            ],
+            exclude: [
+                'node_modules/phaser/src/polyfills/requestAnimationFrame.js',
+                'node_modules/phaser/src/phaser-esm.js'
+            ],
+            sourceMap: false,
+            ignoreGlobal: false
+        }),
+        terser({
+            format: {
+                comments: false
+            }
+        }),
+        del({
+            targets: 'dist/*',
+            runOnce: true
+        }),
+        babel({
+            babelHelpers: 'bundled',
+            exclude: 'node_modules/**'
+        }),
+        url(),
+        copy({
+            targets: [
+                { src: 'index.html', dest: 'dist/' },
+                { src: 'public/*', dest: 'dist/' },
+            ]
+        }),
+        phasermsg()
+    ]
+};
